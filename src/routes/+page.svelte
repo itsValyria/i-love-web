1aed0d669aac7597dd263c441533bc9c7a3f90c1
Valyria <76444716+itsValyria@users.noreply.github.com>
2024-09-13 16:22:49 +0200

    added minimal styles

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,8 +3,32 @@
   let posts = data.posts;
 </script>
 
-<ul>
-  {#each posts as post}
-    <li><a href={`/${post.id}`}>{post.title}</a></li>
-  {/each}
-</ul>
+<main>
+  <ul>
+    {#each posts as post}
+      <li>
+        <a href={`/${post.id}`}>
+          <h2>{post.title}</h2>
+          <p>{post.description}</p>
+        </a>
+      </li>
+    {/each}
+  </ul>
+</main>
+
+<style>
+  ul {
+    margin: 0;
+    padding: 0;
+  }
+
+  li {
+    margin: 1rem 0;
+    padding: 1rem;
+    display: flex;
+    flex-direction: column;
+
+    background-color: var(--rose--400);
+    color: var(--white--000);
+  }
+</style>
