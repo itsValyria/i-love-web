3fc6d108124eaa49226cf81e8fa964904bb2365a
Valyria <76444716+itsValyria@users.noreply.github.com>
2024-10-25 14:08:14 +0200

    change path

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -4,7 +4,7 @@ import path from "path";
 import matter from "gray-matter";
 
 export async function load() {
-  const postsDirectory = path.resolve("./src/lib/posts");
+  const postsDirectory = path.resolve("/src/lib/posts");
 
   try {
     const filenames = fs.readdirSync(postsDirectory);
